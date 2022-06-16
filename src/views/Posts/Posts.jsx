fa7a3a41c367e435ba3ccd7c8cfa907313a84229
Saad <saadsheikh27@gmail.com>
2022-06-16 15:05:18 +0500

    first query

--- src/views/Posts/Posts.jsx
+++ src/views/Posts/Posts.jsx
@@ -1,15 +1,34 @@
 import { useQuery } from "react-query";
 
+import { Loader } from "../../components/Loader";
+import { Card } from "../../components/Card";
 import { getPosts } from "../../api/api.service";
 
 const Posts = () => {
-  const { data, status, error } = useQuery("posts", getPosts);
+  const { data = [], status, error, isFetching } = useQuery("posts", getPosts);
 
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
-      <p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
-    </div>
+    <>
+      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
+        <p className="text-gray-500 text-lg">
+          React and Tailwind CSS in action
+        </p>
+      </div>
+
+      <div className="container mx-auto">
+        {isFetching && (
+          <div className="flex justify-center align-center">
+            <Loader />
+          </div>
+        )}
+
+        {!!data.length &&
+          data.map((item) => {
+            return <Card classes="mb-4" />;
+          })}
+      </div>
+    </>
   );
 };
 
