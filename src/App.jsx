ac9e215ba974631dc37a28c889cc5c116cddd02f
Saad <saadsheikh27@gmail.com>
2022-06-16 13:43:26 +0500

    card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Navbar } from "./components/NavBar";
+import { Card } from "./components/Card";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+
+      <div
+        className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10"
+        onClick={() => set("222")}
+      >
         <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
         <p className="text-gray-500 text-lg">
           React and Tailwind CSS in action
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,31 @@
+const Card = () => {
+  return (
+    <div className="max-w-sm w-full lg:max-w-full lg:flex ml-6">
+      <div className="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className="mb-8">
+          <div className="text-gray-900 font-bold text-xl mb-2">
+            Can coffee make you a better developer?
+          </div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div className="flex items-center">
+          <img
+            className="w-10 h-10 rounded-full mr-4"
+            src="https://images.unsplash.com/photo-1655312432679-1c0b9f77b3b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            // alt="Avatar of Jonathan Reinink"
+          />
+          <div className="text-sm">
+            <p className="text-gray-900 leading-none">Jonathan Reinink</p>
+            <p className="text-gray-600">Aug 18</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
